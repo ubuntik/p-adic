ada9cf8d1d043ef971c0f8589fb440d456a4f999
Anna Subbotina <subbotinanna@gmail.com>
2012-12-04 19:42:43 +0300

    change test6 4 dec 2012

--- tests/test6.c
+++ tests/test6.c
@@ -1,12 +1,14 @@
 #include "../src/p-adic.h"
 
-double p_norm_wrapped(pa_num* pa) { return (double) p_norm(pa); }
-
 double function(pa_num* pa)
 {
 	double ret;
-	ret = p_norm_wrapped(pa) * p_norm_wrapped(pa);
-	return 1 / ret;
+	printf("norma pa = %.16f\n", p_norm(pa));
+	printf("1 / p = %.16f\n", (1.0 / P));
+	printf("diff: %.16f\n", (p_norm(pa) - (1.0 / P)));
+	ret = 1.0 / (p_norm(pa) * p_norm(pa));
+//	return (p_norm(pa) <= (1.0 / P)) ? 0.0 : ret;
+	return ((1.0 / P) >= p_norm(pa)) ? 0.0 : ret;
 }
 
 int main()
@@ -19,13 +21,13 @@ int main()
 	printf("Test#6: Wavelet integrals\n");
 
 	pfunc = function;
-	gmin = -4;
+	gmin = -5;
 	gmax = 0;
 	printf("Parameters: g_max = %d, g_min = %d\n", gmax, gmin);
 	gamma = 0;
 	printf("Current gamma: %d\n", gamma);
-	n = init_pa_num(-4, 0);
-	ret = wavelet_integral(pfunc, n, gamma, 0, gmin, gmax);
+	n = init_pa_num(gmin, gmax);
+	ret = wavelet_integral(pfunc, n, gamma, 1, gmin, gmax);
 	printf(">>>>> Result: %f + i * %f\n\n", crealf(ret), cimagf(ret));
 	free_pa_num(n);
 	return 0;
